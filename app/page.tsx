fdc0e954415b1bd0f104a2af8c38aaee6624eef8
Connor Stevens <conmail1224@gmail.com>
2024-04-22 21:21:31 -0400

    remove padding

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <StarField
         width={window.innerWidth}
         height={window.innerHeight}
